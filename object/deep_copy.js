da54bcf222ca8d55c1333b2dff9b064fa91ec100
adeshkaygude <aadeshkaygude351@gmail.com>
2026-08-24 16:46:04 +0530

    change deep copey

--- object/deep_copy.js
+++ object/deep_copy.js
@@ -1,1 +1,16 @@
+let user = {
+  name: "adesh ",
+  age: 21,
+  address: {
+    city: "pune",
+  },
+};
+
+let str = JSON.stringify(user);
+let user2 = JSON.parse(str);
+
+user.name="new name";
+user.address.city="mumbai"
+console.log(user);
+console.log(user2);
 
