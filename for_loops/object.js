2fd6e0ca725a1254a445723edee85745eeb9d1c9
adeshkaygude <aadeshkaygude351@gmail.com>
2026-08-31 23:47:09 +0530

    object acees using for each

---
+++ for_loops/object.js
@@ -0,0 +1,20 @@
+let arr = [
+  {
+    name: "adesh",
+    age: 21,
+  },
+  {
+    name: "omkar",
+    age: 21,
+  },
+  {
+    name: "prathmesh",
+    age: 21,
+  },
+];
+
+arr.forEach((arr)=>{
+    console.log(arr.name,arr.age);
+    
+    
+})
