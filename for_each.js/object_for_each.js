1ad32c7639fe7db183437d18e363ee01e5f50404
adeshkaygude <aadeshkaygude351@gmail.com>
2026-09-01 18:50:46 +0530

    print the object in js

---
+++ for_each.js/object_for_each.js
@@ -0,0 +1,15 @@
+let arr = [
+  { name: "adesh", marks: 50 },
+  {
+    name: "shradha",
+    marks: 90,
+  },
+  {
+    name: "omkar",
+    marks: 99,
+  },
+];
+
+arr.forEach((student) => {
+  console.log(student);
+});
