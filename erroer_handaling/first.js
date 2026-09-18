8b1ff76fa65113c92fae7b32e7eccf82ef783636
adeshkaygude <aadeshkaygude351@gmail.com>
2026-09-18 23:55:32 +0530

    add  dom file

--- erroer_handaling/first.js
+++ erroer_handaling/first.js
@@ -1,15 +1,9 @@
-function divide(a, b) {
-  if (b === 0) {
-    throw new Error(" can not divided by zero");
-  }
-  return a / b;
-}
-
 try {
-  let result = divide(10, 0);
+  let result = 10 / 0;
   console.log(result);
 } catch (error) {
+  console.log(error.name);
   console.log(error.message);
 } finally {
-  console.log("operation completed");
+  console.log("finaly");
 }
